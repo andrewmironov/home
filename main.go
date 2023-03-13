2247fbaf242f18575a97f87005768199b5854b51
andrewmironov <andrewmironov@idfactory.ru>
2023-03-13 18:45:37 +0300

    1

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func f(i *int) {
+	*i += 1
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Получаем читателя пользовательского ввода
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Interaction counter")
+
+	cnt := 0
+	for {
+		fmt.Print("-> ")
+		// Считываем введённую пользователем строку. Программа ждёт, пока пользователь введёт строку
+		// _, err := reader.ReadString('\n')
+		// if err != nil {
+		// 	panic(err)
+		// }
+
+		f(&cnt)
+
+		fmt.Printf("User input %d lines\n", cnt)
+	}
 }
